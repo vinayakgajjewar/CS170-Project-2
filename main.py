ae4a5def41a9bd3662196431797f1a2eddb604a4
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-02-28 14:04:58 -0800

    read txt file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # main.py
 # This script runs a greedy feature selection algorithm
 
+import csv
+from feature_search import feature_search
+
 print('Welcome to Vinayak Gajjewar\'s feature selection algorithm.')
 
 # Get total number of features from user
@@ -14,3 +17,12 @@ print('\t2) Backward elimination')
 print('\t3) My special algorithm')
 print('Your choice: ', end='')
 alg_number = int(input())
+
+# Read data file
+with open('small-test-dataset.txt') as test_data:
+    data_reader = csv.reader(test_data, delimiter='\t')
+    data_arr = []
+    for row in data_reader:
+        print(row)
+        data_arr.append(row)
+    feature_search(data_arr)
