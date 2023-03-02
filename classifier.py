1a0427117a442ad8d2a393ff25ef443148d1fd16
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-03-02 12:37:07 -0800

    create classifier class

---
+++ classifier.py
@@ -0,0 +1,12 @@
+# classifier.py
+
+class Classifier():
+
+    def __init__(self):
+        print('Initializing classifier')
+
+    def train(self):
+        pass
+
+    def test(self):
+        pass
